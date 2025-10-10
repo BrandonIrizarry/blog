c800b90c72842bbafd0e833b4e57dd7d94782db6
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-10-10 12:33:59 -0400

    Add .dir-locals to repo

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode . ((org-hugo-auto-set-lastmod . t)
+              (org-hugo-base-dir . "~/Desktop/brandonirizarry.github.io/"))))
